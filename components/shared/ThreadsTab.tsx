12baea5d87d0de67536f1b436d5d3547c5ed2cef
Beata Tomczak <tma.btty@gmail.com>
2023-08-06 16:24:29 +0200

    Profile Page - ui

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  currentUserId: string
+  accountId: string
+  accountType: string
+}
+
+const ThreadsTab = ({ currentUserId, accountId, accountType }: Props) => {
+  // TODO: Fetch profile threads
+
+  return <div>ThreadsTab</div>
+}
+
+export default ThreadsTab
